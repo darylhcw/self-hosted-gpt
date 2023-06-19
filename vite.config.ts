d0ebcc01b898937ed371ce6e01ded878806a5e90
Daryl Ho <darylhcw@gmail.com>
2023-06-19 23:41:48 +0800

    Add autoprefixer to solve some vendor css issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import autoprefixer from 'autoprefixer'
+
 import path from 'path';
 
 
@@ -12,6 +14,13 @@ export default defineConfig({
     alias:{
       '@' : path.resolve(__dirname, './src'),
     },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}) // add options if needed
+      ],
+    }
   },
   base: '/self-hosted-gpt/',
 })
